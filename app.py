d8e666a04ead52a9e1dfe13711ad2a3ca3bb0bf8
Nico Epp <nicoeppfriesen@gmail.com>
2019-02-08 11:07:16 -0800

    Documentation and production improvement hints

--- app.py
+++ app.py
@@ -25,6 +25,17 @@ def index():
 
 @app.route('/api/conditions/all')
 def conditions():
+    """
+    Enpoint to get the current weather condition data of all cities at once
+
+    Observations (if used in production):
+    - The output of current_condition() should be cached for some time to not depend
+      on the permformance and availablity of Environment Canada servers.
+    - There could be an endpoint where the just the data of one city could be requested.
+      With that the frontend would be able to parallelize the obtention of the data.
+      For now I replaced a simple for loop with multiprocessing.Pool.map(callback, iterable)
+      which gets an improvement of some seconds. See comment print statement at utils.py
+    """
     try:
         city_weather_list = pool.map(current_condition, CITY_LIST)
     except IOError as e:
--- utils.py
+++ utils.py
@@ -5,6 +5,23 @@ index = CityIndex()
 
 
 def current_condition(cityname):
+    """
+    Helper function to get and format the data per city
+
+    Observations (if used in production):
+    - Maybe inherit and extend City from the cityweather library
+      to make the code more object oriented.
+    - This function could be unit tested with a mockup XML data source
+    - Do more error checks and check also for empty data
+    - Do better messages in case of errors and other unexpected behaviour
+    - Extract (and display) more of the current condition data.
+      The current data points are just a start to show doability.
+    - The cityweather (mini)library should work with streams instead of
+      passing the hole XML string into memory and then parsing it.
+
+    :param cityname: The name of the city spelled exactly like in cityweather.CityIndex.english_city_list()
+    :return: Dictionary structured in a clean format ready and easy to consume by a frontend
+    """
     station_str = 'currentConditions/station'
     condition_str = 'currentConditions/condition'
     temperature_str = 'currentConditions/temperature'
@@ -37,12 +54,16 @@ def current_condition(cityname):
 
 
 def lat_to_float(lat_str):
+    # Examples: "58.42N" to 58.42
+    #           "48.52S" to -48.52
     sense = 1 if lat_str[-1:] == 'N' else -1
     num = float(lat_str[:-1])
     return num * sense
 
 
 def long_to_float(lat_str):
+    # Examples: "130.03W" to -130.03
+    #           "123.03E" to 123.03
     sense = -1 if lat_str[-1:] == 'W' else 1
     num = float(lat_str[:-1])
     return num * sense
